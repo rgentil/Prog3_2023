77a97747b2e8cd8662926bf2b59d3cc26b1e3f3b
Ricardo Gentil <rgentil2112@gmail.com>
2023-03-30 10:36:51 -0300

    Initial commit

---
+++ tp1/ej1/src/main/Main.java
@@ -0,0 +1,40 @@
+package main;
+
+import tp1.MySimpleLinkedList;
+
+/**
+ * @author Gentil Ricardo
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MySimpleLinkedList<Number> list = new MySimpleLinkedList<Number>();
+		System.out.println("Size: " + list.size());
+		System.out.println("Is empty: " + list.isEmpty());
+		
+		list.insertFront(2);
+		System.out.println("Size: " + list.size());
+		System.out.println("Is empty: " + list.isEmpty());
+		
+		System.out.println("ExtractFront: " + list.extractFront());
+		System.out.println("Size: " + list.size());
+		System.out.println("Is empty: " + list.isEmpty());
+		
+		list.insertFront(5);
+		list.insertFront(7);
+		list.insertFront(1);
+		list.insertFront(3);
+		
+		System.out.println("Size: " + list.size());
+		System.out.println("Is empty: " + list.isEmpty());
+		
+		int index = 2;
+		System.out.println("Value in index " + index + ": " + list.get(index));
+		
+		System.out.println(list);
+	}
+
+}
---
+++ tp1/ej1/src/tp1/MySimpleLinkedList.java
@@ -0,0 +1,77 @@
+package tp1;
+/**
+ * 
+ * @author Gentil Ricardo
+ *
+ * @param <T>
+ * 
+ * Ejercicio 1.
+   Implemente los métodos indicados del esqueleto de Lista desarrollado en Teoría
+   (insertFront, extractFront, isEmpty, size, toString). 
+   Agregar también el método: T get(index)
+ */
+public class MySimpleLinkedList<T> {
+	
+	private Node<T> first;
+	private int size;
+	
+	public MySimpleLinkedList() {
+		this.first = null;
+		this.size = 0;
+	}
+	
+	public void insertFront(T info) {
+		Node<T> tmp = new Node<T>(info,null);
+		tmp.setNext(this.first);
+		this.first = tmp;
+		this.size++;
+	}
+	
+	public T extractFront() {		
+		if (!this.isEmpty()) {
+			T info = this.first.getInfo();
+			this.first = this.first.getNext();
+			this.size--;
+			return info;
+		}
+		return null;
+	}
+
+	public boolean isEmpty() {
+		return this.size == 0 && this.first == null;
+	}
+	
+	public T get(int index) {
+		if (!isEmpty() && size() > index) {
+			Node<T> tmp = this.first;
+			for (int i = 0; i <= index && tmp != null; i++) {
+				if (i == index) {
+					return tmp.getInfo();
+				}
+				tmp = tmp.getNext();
+			}
+		}
+		return null;
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	
+	@Override
+	public String toString() {
+		if (isEmpty()) {
+			return "Lista vacia";
+		}
+		System.out.println("-----------Valores en la lista-----------");
+		String resultado = "";
+		Node<T> tmp = this.first;
+		for (int i = 0; i < this.size && tmp != null; i++) {
+			resultado += "Pos " + i + " - Valor " + tmp.getInfo() + " | ";
+			tmp = tmp.getNext();
+		}
+		
+		return resultado;
+	}
+	
+}
